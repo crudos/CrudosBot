36024dec8fac3ee0eeb1f535a6cf949c98568256
Crudos <yemaneg27@gmail.com>
2017-09-23 15:20:48 -0700

    working google sheet integration

--- bot.js
+++ bot.js
@@ -1,39 +1,194 @@
-var Discord = require('discord.io');
-var logger = require('winston');
-var auth = require('./auth.json');
+let Discord = require('discord.io');
+let log = require('winston');
+let auth = require('./auth.json');
+let gs_client = require('./gs_client.js');
 
-logger.remove(logger.transports.Console);
-logger.add(logger.transports.Console, {
-    colorize: true
+log.remove(log.transports.Console);
+log.add(log.transports.Console, {
+   colorize: true
 });
-logger.level = 'debug';
+log.level = 'debug';
 
-var bot = new Discord.Client({
+let bot = new Discord.Client({
    token: auth.token,
    autorun: true
 });
 
 bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
+   log.info('Logged in as: ');
+   log.info(bot.username + ' - (' + bot.id + ')');
 });
 
 bot.on('message', function (user, userID, channelID, message, evt) {
-    if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
+   if(message == 'good job') {
+      bot.sendMessage({ to: channelID, message: 'thanks' });
+   }
 
-        args = args.splice(1);
-        switch(cmd) {
-            // !ping
-            case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Pong!'
-                });
+   let data = {
+      user: user,
+      userID: userID,
+      channelID: channelID,
+      message: message,
+      evt: evt
+   }
+
+   if(message.substring(0, 1) == '.') {
+      data['args'] = message.substring(1).split(' ');
+      data['cmd'] = data.args[0];
+      let messageData = user + ' ' + userID + ' ' + channelID + ' ' + message + ' ' + evt;
+      log.info(messageData)
+
+      switch(data['cmd']) {
+         case 'ping':
+            if(user == 'crudos')  {
+               bot.sendMessage({ to: channelID, message: 'ok' });
+            }
             break;
-            // Just add any case commands if you want to..
-         }
-     }
+         case 'gear':
+            gearCommand(data);
+            break;
+         case 'help':
+            helpCommand(data);
+            break;
+         default:
+            helpCommand(data);
+            break;
+      }
+   }
 });
+
+/** GEAR SCORE COMMANDS **/
+
+let GEAR_SCORE_SPREADSHEET_ID = '1BDDFVjVa9S7c-kZd2U9a9tsnPk0otUAuM7p-UoiS-3A'
+let GEAR_SCORE_SPREADSHEET_RANGE = 'Gear';
+
+function gearCommand(data) {
+   switch(data.args[1]) {
+      case 'show':
+         getGear(data, true);
+         break;
+      case 'update':
+         updateGear(data);
+         break;
+      default:
+         helpCommand(data);
+         break;
+   }
+}
+
+function getGear(data, print=false) {
+   let getParams = {
+      spreadsheetId: GEAR_SCORE_SPREADSHEET_ID,
+      range: GEAR_SCORE_SPREADSHEET_RANGE,
+   };
+
+   return new Promise(function (fulfill, reject) {
+      let promise = gs_client.call(getParams, gs_client.get);
+      promise.then(function(result) {
+         if (print) {
+            gearMessage(data, result);
+         }
+         fulfill(result);
+      }, function(err) {
+         log.error('Error in getGear')
+         reject(err);
+      });
+   });
+}
+
+function gearMessage(data, valueRange) {
+   let msg = '';
+
+   valueRange.values.forEach(function(row) {
+      let rowMsg = '';
+      row.forEach(function(col) {
+         rowMsg += col + '\t\t';
+      })
+      msg += rowMsg.trim() + '\n';
+   });
+
+   bot.sendMessage({ to: data.channelID, message: msg.trim() });
+}
+
+function updateGear(data) {
+   let promise = getGear(data);
+
+   promise.then(function(result) {
+      if (!result) {
+         log.error('Error on updateGear: no sheet data');
+         return;
+      }
+
+      let sheetArray = result.values;
+      let userLowerCase = data.user.toLowerCase();
+      let userRow = null;
+      let rowNum = 0;
+
+      for (i = 0; i < sheetArray.length; i++) {
+         if (sheetArray[i][0].toLowerCase() == userLowerCase) {
+            userRow = i;
+         }
+      }
+
+      let userMessage = data.message;
+      let updateInfo = userMessage.substring(userMessage.indexOf(data['args'][1]) + data['args'][1].length);
+
+      if (updateInfo.length == 0) {
+         helpCommand(data);
+         return;
+      }
+
+      log.info(updateInfo);
+      let sheetColumns = {
+         ap: 1,
+         awk: 2,
+         dp: 3
+      }
+
+      log.info(sheetColumns.ap);
+
+      let sheetUpdate = result;
+      let regex = /(\D+)(\d+)/gi;
+      let updateMatch;
+      while ((updateMatch = regex.exec(updateInfo)) !== null) {
+         let statKey = updateMatch[1];
+         let statVal = updateMatch[2];
+         let columnToUpdate;
+
+         if (statKey.indexOf('ap') != -1) {
+            columnToUpdate = sheetColumns.ap;
+         } else if (statKey.indexOf('awk') != -1) {
+            columnToUpdate = sheetColumns.awk;
+         } else if (statKey.indexOf('dp') != -1) {
+            columnToUpdate = sheetColumns.dp;
+         }
+
+         if (columnToUpdate) {
+            sheetUpdate.values[userRow][columnToUpdate] = statVal;
+         }
+      }
+
+      log.info('found user at ' + userRow);
+      log.info(sheetUpdate.values);
+
+      let updateParams = {
+         spreadsheetId: GEAR_SCORE_SPREADSHEET_ID,
+         range: GEAR_SCORE_SPREADSHEET_RANGE,
+         valueInputOption: 'USER_ENTERED',
+         resource: { values: sheetUpdate.values }
+      }
+
+      gs_client.call(updateParams, gs_client.update).then(log.info);
+   });
+}
+
+function helpCommand(data) {
+   let helpMessage =
+      'Usage: .gear <COMMAND>\n' +
+      '\tshow                  Display gear for players\n' +
+      '\tupdate                Update your gear\n' +
+      '\t\t -ap, --attack Set ap\n' +
+      '\t\t -awk, --awakening Set awakening ap\n' +
+      '\t\t -dp, --defense Set dp';
+   bot.sendMessage({ to: data.channelID, message: helpMessage });
+}
---
+++ gs_client.js
@@ -0,0 +1,200 @@
+// Google Sheets Client
+let fs = require('fs');
+let log = require('winston');
+let readline = require('readline');
+let google = require('googleapis');
+let googleAuth = require('google-auth-library');
+
+let SCOPES = ['https://www.googleapis.com/auth/spreadsheets'];
+let TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
+  process.env.USERPROFILE) + '/.credentials/';
+let TOKEN_PATH = TOKEN_DIR + 'sheets.googleapis.com-nodejs-crudosbot.json';
+
+/**
+ * Init auth client and make GS call
+ */
+exports.call = function(parameters, func) {
+   return new Promise(function (fulfill, reject) {
+      fs.readFile('client_secret.json', function processClientSecrets(err, content) {
+         if(err) {
+            console.log('Error loading client secret file: ' + err);
+            reject(err);
+         }
+
+         let credentials = JSON.parse(content);
+         let clientSecret = credentials.installed.client_secret;
+         let clientId = credentials.installed.client_id;
+         let redirectUrl = credentials.installed.redirect_uris[0];
+         let auth = new googleAuth();
+         let oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
+
+         // Check if we have previously stored a token.
+         fs.readFile(TOKEN_PATH, function(err, token) {
+            if(err) {
+               getNewToken(oauth2Client, callback);
+            } else {
+               oauth2Client.credentials = JSON.parse(token);
+            }
+
+            let params = parameters;
+            params['auth'] = oauth2Client;
+            func(params).then(fulfill);
+         });
+      });
+   });
+}
+
+/** Get call **/
+exports.get = function(params) {
+   log.info('GS - GET');
+
+   return new Promise(function (fulfill, reject) {
+      let sheets = google.sheets('v4');
+      sheets.spreadsheets.values.get(params, function(err, result) {
+         if(err) {
+            log.error(err);
+            reject(err);
+         } else {
+            fulfill(result);
+         }
+      });
+   });
+}
+
+/** Update call **/
+exports.update = function(params) {
+   log.info('GS - UPDATE');
+
+   return new Promise(function (fulfill, reject) {
+      let sheets = google.sheets('v4');
+      sheets.spreadsheets.values.update(params, function(err, result) {
+         if(err) {
+            log.error(err);
+            reject(err);
+         } else {
+            fulfill(result);
+         }
+      });
+   });
+}
+
+/** Helper **/
+
+/**
+ * Get and store new token after prompting for user authorization, and then
+ * execute the given callback with the authorized OAuth2 client.
+ *
+ * @param {google.auth.OAuth2} oauth2Client The OAuth2 client to get token for.
+ */
+function getNewToken(oauth2Client) {
+   let authUrl = oauth2Client.generateAuthUrl({
+      access_type: 'offline',
+      scope: SCOPES
+   });
+
+   console.log('Authorize this app by visiting this url: ', authUrl);
+   let rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+   });
+
+   rl.question('Enter the code from that page here: ', function(code) {
+      rl.close();
+
+      oauth2Client.getToken(code, function(err, token) {
+         if(err) {
+            console.log('Error while trying to retrieve access token', err);
+            return;
+         }
+         oauth2Client.credentials = token;
+         storeToken(token);
+      });
+   });
+}
+
+/**
+ * Store token to disk be used in later program executions.
+ *
+ * @param {Object} token The token to store to disk.
+ */
+function storeToken(token) {
+   try {
+      fs.mkdirSync(TOKEN_DIR);
+   } catch (err) {
+      if(err.code != 'EEXIST') {
+         throw err;
+      }
+   }
+   fs.writeFile(TOKEN_PATH, JSON.stringify(token));
+   console.log('Token stored to ' + TOKEN_PATH);
+}
+
+
+
+/**
+// Load client secrets from a local file.
+fs.readFile('client_secret.json', function processClientSecrets(err, content) {
+ if(err) {
+  console.log('Error loading client secret file: ' + err);
+  return;
+}
+  // Authorize a client with the loaded credentials, then call the
+  // Google Sheets API.
+  authorize(JSON.parse(content), listMajors);
+});
+**/
+
+/**
+ * Create an OAuth2 client with the given credentials, and then execute the
+ * given callback function.
+ *
+ * @param {Object} credentials The authorization client credentials.
+ * @param {function} callback The callback to call with the authorized client.
+ */
+ function authorize(credentials, callback) {
+    let clientSecret = credentials.installed.client_secret;
+    let clientId = credentials.installed.client_id;
+    let redirectUrl = credentials.installed.redirect_uris[0];
+    let auth = new googleAuth();
+    let oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
+
+  // Check if we have previously stored a token.
+  fs.readFile(TOKEN_PATH, function(err, token) {
+     if(err) {
+      getNewToken(oauth2Client, callback);
+   } else {
+      oauth2Client.credentials = JSON.parse(token);
+      callback(oauth2Client);
+   }
+});
+}
+
+/**
+ * Print the names and majors of students in a sample spreadsheet:
+ * https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+ */
+ function listMajors(auth) {
+    let sheets = google.sheets('v4');
+    sheets.spreadsheets.values.get({
+     auth: auth,
+     spreadsheetId: '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms',
+     range: 'Class Data!A2:E',
+  }, function(err, response) {
+     if(err) {
+      console.log('The API returned an error: ' + err);
+      return;
+   }
+   let rows = response.values;
+   if(rows.length == 0) {
+      console.log('No data found.');
+   } else {
+      console.log('Name, Major:');
+      for (let i = 0; i < rows.length; i++) {
+       let row = rows[i];
+        // Print columns A and E, which correspond to indices 0 and 4.
+        console.log('%s, %s', row[0], row[4]);
+     }
+  }
+});
+ }
+
